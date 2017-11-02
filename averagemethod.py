31f9b492462deb5f6799527144a1ad1b4aba621e
Mohd Fitri Alif Bin Mohd Kasai <mohdfitrialif@gmail.com>
2017-11-02 17:50:14 +0800

    Update averagemethod.py

--- averagemethod.py
+++ averagemethod.py
@@ -9,7 +9,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import Adadelta
 from keras.utils import np_utils
-from keras.regularizers import l2, activity_l2
+from keras.regularizers import l2 #, activity_l2
 import numpy as np
 import cPickle 
 import numpy
